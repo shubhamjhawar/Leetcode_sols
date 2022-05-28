7012a8f6625731f909bc4df7e95e6c6d5d224242
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-05-28 10:16:30 +0530

    Time: 22 ms (6.27%), Space: 13.6 MB (84.67%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,72 +1,57 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) 
-    {
+    vector<int> searchRange(vector<int>& nums, int target) {
         
-        if(nums.size() == 0) return {-1 , -1};
-        
-    //first position
-    //predicate is x > = target
-    //ffttttt type
-    // finding first T
+        vector<int> res;
+        //F*T* pattern
+        // arr[mid] >= target
+        //First T is the element we need
         int n = nums.size();
-        int low = 0 ,high = n -1;
-        int mid;
-        vector<int>res;
+        if(nums.size() == 0) return {-1 , -1};
+        int low = 0 ,high = n-1;
         
-        while(low < high)
-        {
-            mid = low + (high - low)/2;
-        
-            if(nums[mid] >= target)  //  Dry run of the example 1 (0 + 5)/2 = 2 then value of low becomes 3 , 5 mid is the low == 4 high = 4,then in the last iteration we get the result as follows;
-            {
-              high = mid;
+        while(low < high){
+            
+           int mid =  low + (high-low)/2;
+            
+            if(nums[mid] >= target){
+                high = mid;
             }
-            else
-                low = mid+ 1;
-            
+            else{
+                low = mid+1;
+            }
+             
+        }
+        if(nums[low] == target){
+            res.push_back(low);
+        }
+        else{
+            return {-1,-1};
         }
         
-        if(nums[low] !=  target )
-        {
-            return { -1 , -1};
-            
-        }
         
-        else
-        {  
+        //F*T*
+        //arr[mid] > target
+        
+        low = 0 ;
+        high = n-1;
+        
+        while(low < high){
+            
+           int mid =  low + (high-low+1)/2;
+            
+            if(nums[mid] > target){
+                high = mid-1;
+            }
+            else{
+                low = mid;
+            }
+             
+        }
+        if(nums[low] == target){
             res.push_back(low);
         }
         
-        
-        //last position
-        // FFTTTTT
-        //nums[x] > target;
-        
-        
-           low = 0, high = n -1;
-        
-        while(low < high)
-        {
-          
-            mid = low + (high - low + 1)/2;// 0 and 5 is index in eg 1; low = 3 index and high is still 5 ; low becomes equal to 4;
-                
-           if( nums[mid] > target)
-           {
-               high = mid - 1;
-           }
-            else 
-                low = mid;
-        
-        
-        }
-        
-        //sanity check
-        
-        res.push_back(low);
-        
-        return res;
-    
-    }  
-    
+        return res; 
+    }
 };
