d9134a2e95c43d607b8542d8bb33cc70a28ffa0e
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-05-28 17:41:07 +0530

    Time: 0 ms (100.00%), Space: 6 MB (92.33%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     int helper(string& s,int index,vector<int>& dp){
-        int n = s.size();
-        //Base Case
-        if(index == n) return 1;
-        if(s[index]=='0')return 0;
-        if(index == n-1) return 1;
+        int n = index;
+        dp[n%2] = 1;
+        dp[(n-1)%2] = s[n-1] == '0' ? 0 : 1;
         
-        
-        int lh = 0;
-        int rh = 0;
-        
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        
+      for(int i = n-2 ; i >= 0 ; i--){
+         int temp = 0;
         //Recursive case
-        if(s[index] != '0'){
-            lh = helper(s,index+1,dp);
+        if(s[i] != '0'){
+            temp += dp[(i+1)%2];
         }
         
-        if(index + 1 < n && s[index] != '0' && (s[index] == '1' || s[index] == '2' && s[index+1] <= '6' )){
-            rh = helper(s,index+2,dp);
+        if(s[i] == '1' || s[i] == '2' && s[i+1] <= '6' ){
+            temp += dp[(i+2)%2];
         }
+          
+        dp[i%2] = temp;
         
+      }
         
-        return dp[index] = (lh + rh);
+      return dp[0%2];
+        
     }
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(2);
      
-        return helper(s,0,dp);
+        return helper(s,n,dp);
         
     }
 };
