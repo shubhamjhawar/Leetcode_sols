231b2aa14622120d37d33d84708d2f34acedb655
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-05-30 21:35:09 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.45%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend == divisor){
+            return 1;
+        }
+        bool isPositive = (dividend<0 == divisor<0);
+        unsigned int a = abs(dividend);
+        unsigned int b = abs(divisor);
+        unsigned int ans = 0;
+        while(a >= b){
+            short q = 0;
+            
+            while(a > (b<<q+1)){
+                q++;
+            }
+            
+            ans += 1<<q;  // add the power of 2 found to the answer
+            a = a - (b<<q); 
+        }
+        
+        if(ans == (1<<31) and isPositive)   // if ans cannot be stored in signed int
+            return INT_MAX;
+        return isPositive ? ans : -ans;
+        
+    }
+};
