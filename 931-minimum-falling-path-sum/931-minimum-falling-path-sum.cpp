f98806ebd7356e9e914470dc76137e3e5aa54c1c
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-05 16:20:08 +0530

    Time: 32 ms (12.03%), Space: 10.3 MB (23.11%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-  int getMaxUtil(int i, int j, int m, vector<vector<int>> &matrix, 
-vector<vector<int> > &dp){
-    
-    // Base Conditions
-    if(j<0 || j>=m)
-        return 1e9;
-    if(i==0)
-        return matrix[0][j];
-    
-    if(dp[i][j]!=-100005) return dp[i][j];
-    
-    int up = matrix[i][j] + getMaxUtil(i-1,j,m,matrix,dp);
-    int leftDiagonal = matrix[i][j] + getMaxUtil(i-1,j-1,m,matrix,dp);
-    int rightDiagonal = matrix[i][j] + getMaxUtil(i-1,j+1,m,matrix,dp);
-    
-    return dp[i][j]= min(up,min(leftDiagonal,rightDiagonal));
-    
-}
+ int f(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
+        int n = matrix.size();
+        //Base case
+        if(j < 0 || j >= n)
+            return 1e9;
+        if(i == 0) return 
+            matrix[0][j];
+        
+        if(dp[i][j] != 100005){
+            return dp[i][j];
+        }
+        
+        //Recursive case
+        int up = f(i-1,j,matrix,dp) + matrix[i][j];
+        int ld = f(i-1,j-1,matrix,dp)  + matrix[i][j];
+        int rd = f(i-1,j+1,matrix,dp)  + matrix[i][j];
+        
+        return dp[i][j] =  min(up,min(ld,rd));
+        
+    }
  int minFallingPathSum(vector<vector<int>>& matrix) {
     int n = matrix.size();
     //int m = matrix[0].size();
     
-    vector<vector<int>> dp(n,vector<int>(n,-100005));
+    vector<vector<int>> dp(n,vector<int>(n,100005));
     
     int maxi = INT_MAX;
     
     for(int j=0; j<n;j++){
-        int ans = getMaxUtil(n-1,j,n,matrix,dp);
+        int ans = f(n-1,j,matrix,dp);
         maxi = min(maxi,ans);
     }
     
