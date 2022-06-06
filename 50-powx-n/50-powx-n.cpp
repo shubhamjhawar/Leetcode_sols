3e930077563a222751001defa06578e70105379c
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-06 14:41:57 +0530

    Time: 3 ms (43.73%), Space: 5.9 MB (44.61%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long nn = n;
+        double ans = 1.0;
+        if(n  < 0) nn *= -1;
+        while(nn){
+            if(nn%2 == 0){
+                x = x*x;
+                nn = nn/2;
+            }
+            else{
+                ans = ans*x;
+                nn = nn-1;
+            }
+        }
+        
+        if(n < 0) ans = (double)1.0/(double)ans;
+        return ans;
+        
+    }
+};
