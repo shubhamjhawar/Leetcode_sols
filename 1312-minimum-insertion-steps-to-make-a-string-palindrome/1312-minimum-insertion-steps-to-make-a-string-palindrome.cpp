9e49e4db621fa8c3a5cec90c944ef582ff800597
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-13 12:39:32 +0530

    Time: 117 ms (17.07%), Space: 27.2 MB (24.95%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,63 +1,32 @@
 class Solution {
 public:
-//     int lcs(string& s1,string& s2,int n){
+    int lcs(string& s1,string& s2,int n){
         
-//         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-//         int i,j;
-//         for(i = 0 ; i <= n ; i++) dp[i][0] = 0;
-//         for(j = 0 ; j <= n ; j++) dp[0][j] = 0;
-        
-//         for(i  = 1 ; i <= n ; i++){
-            
-//             for(j = 1;j <= n ; j++){
-                
-//                 if(s1[i-1] == s1[j-1]){
-//                     dp[i][j] = 1 + dp[i-1][j-1];
-//                 }
-//                 else{
-//                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-//                 }
-//             }
-//         }
-        
-//         return dp[n][n];
-//     }
-    
-  int f(int n1,int n2,vector<vector<int>>& dp,string& text1, string& text2){
-        
-        //Base case
-      
-       for(int j = 0 ; j <= n2 ; j++){
-           dp[0][j] = 0;
-       }
-        
-      for(int i = 1 ; i <= n1 ; i++){
-          dp[i][0] = 0;
-          for(int j = 1 ; j <= n2 ; j++){
-              if(text1[i-1] == text2[j-1]){
-                  dp[i][j] = 1 + dp[(i-1)][j-1];
-              }
-              else{
-                  dp[i][j] = max(dp[i][j-1],dp[(i-1)][j]);
-              }
-          }
-      }
-
-     return dp[n1][n2];
-         
-    }
-    int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        string s1 = s;
-        reverse(s1.begin(),s1.end());
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        int i,j;
+        for(i = 0 ; i <= n ; i++) dp[i][0] = 0;
+        for(j = 0 ; j <= n ; j++) dp[0][j] = 0;
         
-        return f(n,n,dp,s,s1);
+        for(i  = 1 ; i <= n ; i++){
+            
+            for(j = 1;j <= n ; j++){
+                
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
         
+        return dp[n][n];
     }
     int minInsertions(string s) {
-        
-        int len = longestPalindromeSubseq(s);
+        string s1 = s;
+        int n = s.size();
+        reverse(s1.begin(),s1.end());
+        int len = lcs(s,s1,n);
         return s.size() - len;
     }
 };
