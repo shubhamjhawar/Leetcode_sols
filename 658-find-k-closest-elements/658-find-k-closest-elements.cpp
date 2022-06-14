7d41155718cfc83a33b28a778f7b961eec298738
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-14 19:07:56 +0530

    Time: 189 ms (7.44%), Space: 34.3 MB (16.28%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,13 +1,25 @@
+typedef pair<int,int> pii;
 class Solution {
 public:
         vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> nums = arr;
-        stable_sort(begin(nums), end(nums), [x](const auto a, const auto b){
-            return abs(a - x) < abs(b - x);   // sort by distance from x
-        });
-        nums.resize(k);                        // choose first k elements
-        sort(begin(nums), end(nums));           // sort the output in ascending order before returning
-        return nums;
+        priority_queue<pii> pq;
+        vector<int> res;
+        for(int i = 0 ; i < arr.size() ; i++){
+            pq.push(make_pair(abs(arr[i] - x),arr[i]));
+            
+            if(pq.size() > k){
+                pq.pop();
+            }   
+        }
+        while(!pq.empty()){
+            res.push_back(pq.top().second);
+                pq.pop();
+        }
+            
+        stable_sort(res.begin(),res.end());
+        
+        return res;
+        
     }
     
 };
