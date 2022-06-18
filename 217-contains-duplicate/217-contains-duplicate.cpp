6fd1684dccfc22a53fdd416dcf4f67e905894502
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-18 22:18:27 +0530

    Time: 127 ms (56.61%), Space: 53.4 MB (22.21%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:        
+      bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int>s;
+        for(int i = 0; i < nums.size(); i++) 
+            s.insert(nums[i]);
+          
+        if(nums.size() != s.size()) return true;
+        return false;   
+    }
+};
