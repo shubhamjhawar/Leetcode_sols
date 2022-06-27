b8112d2e1676c3f8c4cf842b2cbba31f2439dfa3
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-27 15:26:29 +0530

    Time: 3 ms (97.77%), Space: 17.3 MB (37.92%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+   
+    
+   void f(vector<int>& candidates,int start , int end , int target,vector<int>& contri, vector<vector<int>>& res) {
+       
+       
+       //Base Case
+       if(target == 0){
+           res.push_back(contri);
+           return;
+       }
+       
+       if(target < 0){
+           return;
+       }
+       
+       if(start > end){
+           return;
+       }
+       
+       
+       
+       
+       //Recursive case
+       //Not Take case
+       f(candidates,start+1,end,target,contri,res);
+       
+       //take case
+       if(candidates[start] <= target){
+           contri.push_back(candidates[start]);
+           f(candidates,start,end,target - candidates[start],contri,res);
+           contri.pop_back();
+       }
+      
+   }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> res;
+        vector<int> contri;
+        f(candidates,0,candidates.size()-1,target,contri,res);
+        return res;
+    }
+};
