9818e7557d778ddafce84a625caa1aceaeaa9f3e
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-06-28 11:34:11 +0530

    Time: 40 ms (7.14%), Space: 10.3 MB (24.23%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& matrix,vector<vector<int>>& dp){
-       int n = matrix.size();
-       for(int k = 0 ;k < n ; k++){
-           dp[0%2][k] = matrix[0%2][k];
-       }
-        
-       for(int i = 1 ; i < n ;i++){
-           for(int j = 0 ; j < n ; j++){
-               int up = 1e9,ld = 1e9 ,rd = 1e9;
-                up = dp[(i-1)%2][j] + matrix[i][j];
-               if(j-1 >= 0){
-                   ld = dp[(i-1)%2][j-1] + matrix[i][j];
-               }
-               if(j+1 < n){
-                   rd = dp[(i-1)%2][j+1] + matrix[i][j];
-               }
-               
-               dp[i%2][j] = min(up,min(ld,rd));
-           }
-       }
-       int min_elt = INT_MAX;
-       for(int i = 0 ; i < n ; i++){
-           min_elt = min(min_elt,dp[(n-1)%2][i]);
-       }
-       return min_elt;
+  int getMaxUtil(int i, int j, int m, vector<vector<int>> &matrix, 
+vector<vector<int> > &dp){
+    
+    // Base Conditions
+    if(j<0 || j>=m)
+        return 1e9;
+    if(i==0)
+        return matrix[0][j];
+    
+    if(dp[i][j]!=-1) return dp[i][j];
+    
+    int up = matrix[i][j] + getMaxUtil(i-1,j,m,matrix,dp);
+    int leftDiagonal = matrix[i][j] + getMaxUtil(i-1,j-1,m,matrix,dp);
+    int rightDiagonal = matrix[i][j] + getMaxUtil(i-1,j+1,m,matrix,dp);
+    
+    return dp[i][j]= min(up,min(leftDiagonal,rightDiagonal));
+    
+}
+ int minFallingPathSum(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    
+    int maxi = INT_MAX;
+    
+    for(int j=0; j<m;j++){
+        int ans = getMaxUtil(n-1,j,m,matrix,dp);
+        maxi = min(maxi,ans);
     }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>> dp(2,vector<int>(n,100005));
-        return f(matrix,dp);
-
+    
+    return maxi;
     }
 };
