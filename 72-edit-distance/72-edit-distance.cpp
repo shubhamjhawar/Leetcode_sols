f09ae453428ff78f19286c31f6c024865f14148f
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-08 18:09:31 +0530

    Time: 15 ms (71.32%), Space: 9 MB (56.73%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-   int editDistanceUtil(string& S1, string& S2, int i, int j, vector<vector<int>>& dp){
+    int f(string& s1,string& s2,int i ,int j,vector<vector<int>>& dp){
+        
+        //Base Case
+        if(i < 0){
+           return j+1;
+        }
+        
+        if(j < 0){
+            return i+1;
+        }
     
-    if(i<0)
-        return j+1;
-    if(j<0)
-        return i+1;
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
         
-    if(dp[i][j]!=-1) return dp[i][j];
-        
-    if(S1[i]==S2[j])
-        return dp[i][j] =  0+editDistanceUtil(S1,S2,i-1,j-1,dp);
-        
-    // Minimum of three choices
-    else return dp[i][j] = 1+min(editDistanceUtil(S1,S2,i-1,j-1,dp),
-    min(editDistanceUtil(S1,S2,i-1,j,dp),editDistanceUtil(S1,S2,i,j-1,dp)));
-    
-}
+        //Recursive case
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 0 + f(s1,s2,i-1,j-1,dp);
+        }
+        else{
+            return dp[i][j] = 1+min(f(s1,s2,i,j-1,dp),min(f(s1,s2,i-1,j,dp),f(s1,s2,i-1,j-1,dp)));
+        }
+
+    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
         
         vector<vector<int>>dp(n,vector<int>(m,-1));
         
-        return editDistanceUtil(word1,word2,n-1,m-1,dp);
+        return f(word1,word2,n-1,m-1,dp);
         
     }
 };
