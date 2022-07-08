97e07066b22291e2dc5ac0356bb8ccdc17560b7c
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-08 18:19:18 +0530

    Time: 21 ms (51.67%), Space: 9 MB (38.28%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
-    int f(string& s1,string& s2,int i ,int j,vector<vector<int>>& dp){
+    int f(string& s1,string& s2,int m ,int n,vector<vector<int>>& dp){
         
-        //Base Case
-        if(i == 0){
-           return j;
+        
+        
+        for(int j = 0 ; j <= n ; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 0 ; i <= m ; i++){
+            dp[i][0] = i;
         }
         
-        if(j == 0){
-            return i;
-        }
+        
+        for(int i = 1; i <= m ; i++){
+            for(int j = 1 ; j <= n ;j++){
+                    if(s1[i-1] == s2[j-1]){
+                       dp[i][j] = 0 + dp[i-1][j-1];
+                     }
+                     else{
+                       dp[i][j] = 1+min(dp[i-1][j] ,min(dp[i-1][j-1],dp[i][j-1]));
+                     }
+                
+                 }     
+               }
+        
+        return dp[m][n];
+}
+        
+//         //Base Case
+//         if(i == 0){
+//            return j;
+//         }
+        
+//         if(j == 0){
+//             return i;
+//         }
     
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
         
-        //Recursive case
-        if(s1[i-1] == s2[j-1]){
-            return dp[i][j] = 0 + f(s1,s2,i-1,j-1,dp);
-        }
-        else{
-            return dp[i][j] = 1+min(f(s1,s2,i,j-1,dp),min(f(s1,s2,i-1,j,dp),f(s1,s2,i-1,j-1,dp)));
-        }
+//         //Recursive case
+//         if(s1[i-1] == s2[j-1]){
+//             return dp[i][j] = 0 + f(s1,s2,i-1,j-1,dp);
+//         }
+//         else{
+//             return dp[i][j] = 1+min(f(s1,s2,i,j-1,dp),min(f(s1,s2,i-1,j,dp),f(s1,s2,i-1,j-1,dp)));
+//         }
 
-    }
+//     }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
