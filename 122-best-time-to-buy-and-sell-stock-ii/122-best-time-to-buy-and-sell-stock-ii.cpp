fe684aa0c1e6596d5bf2cba64f31212b8e8a23b4
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-09 12:59:32 +0530

    Time: 11 ms (54.74%), Space: 13.1 MB (54.89%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int f(int n,int option,vector<vector<int>>& dp,vector<int>& prices){
-         dp[n][0] = 0,dp[n][1] = 0;
+         dp[n%2][0] = 0,dp[n%2][1] = 0;
         
          for(int i = n-1; i >= 0 ; i--){
-               dp[i][1] = max(-prices[i] + dp[i+1][0],dp[i+1][1]);
-               dp[i][0] = max( prices[i] + dp[i+1][1],dp[i+1][0]);
+               dp[i%2][1] = max(-prices[i] + dp[(i+1)%2][0],dp[(i+1)%2][1]);
+               dp[i%2][0] = max( prices[i] + dp[(i+1)%2][1],dp[(i+1)%2][0]);
          }
         
         
-        return dp[0][1];
+        return dp[0%2][1];
 
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        vector<vector<int>> dp(2,vector<int>(2,-1));
         return f(n,1,dp,prices);
     }
 };
