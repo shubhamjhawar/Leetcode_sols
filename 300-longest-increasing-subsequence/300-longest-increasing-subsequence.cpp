10230c5717adc7e6a9806d4d0e0e3c90cf52d141
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-11 15:00:44 +0530

    Time: 993 ms (12.09%), Space: 10.9 MB (17.58%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -3,27 +3,26 @@ public:
     int LIS(vector<int>& nums,vector<vector<int>>& dp){
         
         int n = nums.size();
-    
         for(int index = n-1 ; index >= 0 ; index--){
             for(int prev_index = index-1 ; prev_index >= -1 ; prev_index--){
                 //Not take
-                int len = 0 + dp[index+1][prev_index+1];
+                int len = 0 + dp[(index+1)%2][prev_index+1];
                 
                 //Take case
                 if(prev_index == -1 || nums[index] > nums[prev_index]){
-                     len = max(len,1 + dp[index+1][index+1]);
+                     len = max(len,1 + dp[(index+1)%2][index+1]);
                 }
-                 dp[index][prev_index+1] = len;
+                 dp[(index)%2][prev_index+1] = len;
             }
         }
         
         
-        return dp[0][0];
+        return dp[0%2][0];
      
 }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<vector<int>>dp(2,vector<int>(n+1,0));
         return LIS(nums,dp);
     }
 };
