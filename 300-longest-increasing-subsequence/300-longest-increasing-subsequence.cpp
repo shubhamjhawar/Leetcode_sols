c03b903afe64091c736ebc158237dc16281a0415
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-15 16:15:08 +0530

    Time: 16 ms (78.19%), Space: 10.4 MB (67.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,55 +1,19 @@
 class Solution {
 public:
-    int LIS(vector<int>& nums,vector<int>& dp){
-        
-        int n = nums.size();
-        int max_len = 1;
-        vector<int> hash(n);
-        
-       int last_index = 0;
-       for(int index = 0 ; index < n ; index++){
-           hash[index] = index;
-           for(int prev_index = 0 ; prev_index < index ; prev_index++){
-               if(nums[prev_index] < nums[index] and dp[index] < 1 + dp[prev_index]){
-                   dp[index] = 1 + dp[prev_index];
-                   
-                   hash[index] = prev_index;
-               }
-           }
-           if(dp[index] > max_len){
-               max_len = dp[index];
-               last_index = index;
-           }
-       } 
-        
-      cout << last_index << endl;
-      // for(int i = 0 ; i < n ; i++){
-      //     cout << hash[i] << " ";
-      // }
-        
-      vector<int> temp;
-      temp.push_back(nums[last_index]);
-        
-        
-      while(hash[last_index] != last_index){
-          last_index = hash[last_index];
-          temp.push_back(nums[last_index]);
-      }
-      
-      reverse(temp.begin(),temp.end());
-        
-      for(auto it : temp){
-          cout << it << " ";
-      }
-     
+int lengthOfLIS(vector<int>& nums) {
+        vector<int> temp;
+        temp.push_back(nums[0]);
     
-      cout << endl;
-      return max_len;
-     
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,1);
-        return LIS(nums,dp);
+         for(int i = 0 ; i < nums.size() ; i++){
+             if(nums[i] > temp.back()){
+                 temp.push_back(nums[i]);
+             }
+             else{
+                 int index = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin();
+                 temp[index] = nums[i];
+                 
+             }
+         }
+         return temp.size();
     }
 };
