4e1edb418e54a0efb12e29d3c2f6c8f1ab376a26
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-22 07:07:30 +0530

    Time: 19 ms (69.23%), Space: 21.4 MB (76.70%) - LeetHub

---
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructTree(vector<int>& nums,int left , int right){
+        
+        if(left > right){
+            return NULL;
+        }
+        
+        int mid = left + (right - left + 1)/2;
+        TreeNode* curr = new TreeNode(nums[mid]);
+        curr->left = constructTree(nums,left,mid-1);
+        curr->right = constructTree(nums,mid+1,right);
+        
+        return curr;
+        
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if(nums.size() == 0) return NULL;
+        return constructTree(nums,0,nums.size()-1);
+    }
+    
+    
+    
+    //    0  1    2    3   4  5  6 
+    // [-10 ,-5 ,-3 ,0 , 5, 8, 9]   //indices
+    //Calculate the middle element
+    // 0 + (7 - 1)/2; = 3
+    
+         
+    
+};
