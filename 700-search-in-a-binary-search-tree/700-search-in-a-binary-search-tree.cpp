641c7dd2b05badbb32f044115e868197e43c62ce
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-24 19:34:26 +0530

    Time: 44 ms (90.10%), Space: 34.8 MB (66.34%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -1,23 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-  TreeNode* helper(TreeNode* root,int val){
-      if(!root){
-          return  NULL;
-      }
-      
-      if(root->val == val){
-          return root;
-      }
-      
-      if(root->val > val){
-         return  helper(root->left,val);
-      }
-         
-       return helper(root->right,val);
-    
-  }
   TreeNode* searchBST(TreeNode* root, int val) {
     
-     return helper(root,val);
+      while(root){
+          if(root->val == val){
+              return root;
+          }
+          
+          if(root->val > val){
+              root = root->left;
+          }
+          else{
+              root = root->right;
+          }
+      }
+      return root;
   }
 };
