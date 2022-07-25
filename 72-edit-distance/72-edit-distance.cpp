02f3ae2b523a616c5ab11e9b67f13e6dccf1dad4
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-25 11:28:22 +0530

    Time: 6 ms (95.72%), Space: 9.1 MB (21.08%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,61 +1,35 @@
 class Solution {
 public:
-    int f(string& s1,string& s2,int m ,int n,vector<vector<int>>& dp){
+    int f(int i ,int j ,string& word1, string& word2,vector<vector<int>>& dp){
+        
+        if(i == 0) return j;
+        if(j == 0) return i;
         
         
-        
-        for(int j = 0 ; j <= n ; j++){
-            dp[0][j] = j;
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        for(int i = 0 ; i <= m ; i++){
-            dp[i][0] = i;
+        
+        //Recursive case
+        if(word1[i-1] == word2[j-1]){
+            return dp[i][j] =  0 + f(i-1,j-1,word1,word2,dp);
         }
         
+        //Insert 
+        int ins = f(i,j-1,word1,word2,dp) + 1;
         
-        for(int i = 1; i <= m ; i++){
-            for(int j = 1 ; j <= n ;j++){
-                    if(s1[i-1] == s2[j-1]){
-                       dp[i][j] = 0 + dp[i-1][j-1];
-                     }
-                     else{
-                       dp[i][j] = 1+min(dp[i-1][j] ,min(dp[i-1][j-1],dp[i][j-1]));
-                     }
-                
-                 }     
-               }
+        //Delete
+        int del = f(i-1,j,word1,word2,dp) + 1;
         
-        return dp[m][n];
-}
+        //Replace
+        int rep = f(i-1,j-1,word1,word2,dp) + 1;
         
-//         //Base Case
-//         if(i == 0){
-//            return j;
-//         }
-        
-//         if(j == 0){
-//             return i;
-//         }
-    
-//         if(dp[i][j] != -1){
-//             return dp[i][j];
-//         }
-        
-//         //Recursive case
-//         if(s1[i-1] == s2[j-1]){
-//             return dp[i][j] = 0 + f(s1,s2,i-1,j-1,dp);
-//         }
-//         else{
-//             return dp[i][j] = 1+min(f(s1,s2,i,j-1,dp),min(f(s1,s2,i-1,j,dp),f(s1,s2,i-1,j-1,dp)));
-//         }
-
-//     }
+        return dp[i][j] = min(ins,min(del,rep));
+    }
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
-        return f(word1,word2,n,m,dp);
-        
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        return f(word1.size(),word2.size() ,word1,word2,dp);
     }
 };
