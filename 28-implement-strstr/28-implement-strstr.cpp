5333a37cc53ee696b3adb5a9703f0560f67bc25f
shubhamjhawar <66582610+shubhamjhawar@users.noreply.github.com>
2022-07-26 13:02:51 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (61.13%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -15,10 +15,17 @@ public:
         int k = needle.size();
         int i = 0, j = 0;
         bool flag = false;
+        
+        //Sliding Window Technique as a whole
         while(j < haystack.size()){
+            
+            //Keep sliding the window till it
+            //becomes the appropriate size
             if(j - i + 1 < k){
                 j++;
             }
+            //if Window size == k
+            //Check if the first element itself is not equal 
             else if(j - i + 1 == k){
                 if(haystack[i] != needle[0]){
                     i++;
